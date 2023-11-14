3540ebb0f07ba143fc7671f006087cc1c5c2c939
ardbek <149989995+ardbek@users.noreply.github.com>
2023-11-14 17:47:21 +0900

    [Silver IV] Title: 제로, Time: 764 ms, Memory: 281956 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,30 +1,29 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
 
-        int temp = 0;
-        int sum = 0;
-        int[] arr = new int[n];
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < n; i++) {
-            temp = Integer.parseInt(br.readLine());
-            if (temp == 0) {
-                for (int j = n-1; j >= 0; j--) {
-                    if (arr[j] != 0) {
-                        sum -= arr[j];
-                        arr[j] = 0;
-                        break;
-                    }
-                }
+        int K = Integer.parseInt(sc.nextLine());
+
+        for (int i = 0; i < K; i++) {
+            int number = Integer.parseInt(sc.nextLine());
+
+            if (number == 0) {
+                stack.pop();
+            } else {
+                stack.push(number);
             }
-            arr[i] = temp;
-            sum += temp;
+        }
+
+        int sum = 0;
+        for (int o : stack) {
+            sum += o;
         }
         System.out.println(sum);
     }
