1e3e81631aaa121e379736c3451aba863441dc89
ardbek <149989995+ardbek@users.noreply.github.com>
2023-11-22 17:16:41 +0900

    [Bronze V] Title: A+B - 2, Time: 80 ms, Memory: 11460 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(bf.readLine())+Integer.parseInt(bf.readLine()));
+
+    }
+}
