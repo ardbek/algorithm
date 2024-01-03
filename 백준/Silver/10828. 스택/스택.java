f12988abac0e2119bed913f6761216189f6414fd
ardbek <149989995+ardbek@users.noreply.github.com>
2024-01-03 13:46:57 +0900

    [Silver IV] Title: 스택, Time: 136 ms, Memory: 16756 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -5,21 +5,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        String cmd = "";
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            cmd = st.nextToken();
+            String cmd = st.nextToken();
 
             switch (cmd) {
                 case "push":
                     stack.push(Integer.parseInt(st.nextToken()));
                     break;
                 case "pop":
-                    if (stack.empty()) {
+                    if (stack.isEmpty()) {
                         sb.append("-1").append("\n");
                     } else {
                         sb.append(stack.pop()).append("\n");
@@ -29,14 +29,14 @@ public class Main {
                     sb.append(stack.size()).append("\n");
                     break;
                 case "empty":
-                    if (stack.empty()) {
+                    if (stack.isEmpty()) {
                         sb.append("1").append("\n");
                     } else {
                         sb.append("0").append("\n");
                     }
                     break;
                 case "top":
-                    if (stack.empty()) {
+                    if (stack.isEmpty()) {
                         sb.append("-1").append("\n");
                     } else {
                         sb.append(stack.peek()).append("\n");
@@ -45,8 +45,7 @@ public class Main {
             }
 
         }
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
 
     }
-
 }
