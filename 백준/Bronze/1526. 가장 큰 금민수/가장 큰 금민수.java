2fdb4650ac0553188dc0f07bde37197a596a2055
ardbek <149989995+ardbek@users.noreply.github.com>
2024-01-09 17:49:41 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 104 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.java
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.java
@@ -1,10 +1,11 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int result = 0;
 
         for (int i = 4; i <= n; i++) {
